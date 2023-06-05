54eca898657ea421dd2d2a6e9699b290e16f43da
Majed Althonayan <majed@Majeds-MacBook-Pro.local>
2023-06-05 17:51:53 +0100

    Made the first page for the portfolio

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
-
 export default function Home() {
 	return (
-		<div>
-			<h1>Hello</h1>
+		<div className="pt-40 flex flex-col items-center">
+			<h1 className="text-center helloTitle">
+				Hello, I'm {"\n"}
+				<h1 className="name">Majed Althonayan</h1>
+			</h1>
+			<h3 className="text-center myDescription">
+				A Computer Science student based out of London, England
+			</h3>
+			<button className="contactMe rounded-md  pl-3 pr-3 pt-2 pb-2 ...">
+				Save Changes
+			</button>
 		</div>
 	);
 }
